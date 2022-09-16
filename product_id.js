e64a5607d1eb440fdefdf8e592c6ebe03ca02fcf
Sumit <sumitgupta19900@gmail.com>
2022-09-16 20:16:20 +0530

    Nandini chikan website

---
+++ product_id.js
@@ -0,0 +1,31 @@
+
+var productimg = document.getElementById("productimg");
+var smallimg = document.getElementsByClassName("small-img");
+console.log(smallimg)
+smallimg[0].onclick = function () {
+    productimg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function () {
+    productimg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function () {
+    productimg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    productimg.src = smallimg[3].src;
+}
+smallimg[4].onclick = function () {
+    productimg.src = smallimg[4].src;
+}
+smallimg[5].onclick = function () {
+    productimg.src = smallimg[5].src;
+}
+smallimg[6].onclick = function () {
+    productimg.src = smallimg[6].src;
+}
+smallimg[7].onclick = function () {
+    productimg.src = smallimg[7].src;
+}
+smallimg[8].onclick = function () {
+    productimg.src = smallimg[8].src;
+}
